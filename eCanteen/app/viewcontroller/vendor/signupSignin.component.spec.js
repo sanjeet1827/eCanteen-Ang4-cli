e96b34df5c3988159f6eee5ce90ae8b13277b98b
Sanjeet Chauhan <sanjeet1827@gmail.com>
2018-02-21 16:21:13 +0530

    test casses testing

--- eCanteen/app/viewcontroller/vendor/signupSignin.component.spec.js
+++ eCanteen/app/viewcontroller/vendor/signupSignin.component.spec.js
@@ -8,18 +8,33 @@ var textboxgeneral_component_1 = require("../../directives/textboxgeneral.compon
 var button_component_1 = require("../../directives/button.component");
 var signupSignin_component_1 = require("./signupSignin.component");
 var Signin_component_1 = require("./Signin.component");
+var site_service_1 = require("../../datacontext/site.service");
+var vendor_service_1 = require("../../datacontext/vendor.service");
+var httpHelper_1 = require("../../Helpers/httpHelper");
 describe('SignupSinginComponent (template)', function () {
     var comp;
     var fixture;
     var de;
     var el;
+    var spy;
+    var testSites;
+    var testLoginVendor;
     beforeEach(function () {
         testing_1.TestBed.configureTestingModule({
             imports: [forms_1.FormsModule, platform_browser_2.BrowserModule],
             declarations: [signupSignin_component_1.SignupSinginComponent, Signin_component_1.SinginComponent, textboxgeneral_component_1.TextboxGeneralComponent, button_component_1.ButtonComponent],
+            providers: [
+                site_service_1.SiteService,
+                vendor_service_1.VendorService,
+                httpHelper_1.httpHelper
+            ]
         });
         fixture = testing_1.TestBed.createComponent(signupSignin_component_1.SignupSinginComponent);
         comp = fixture.componentInstance; // BannerComponent test instance
+        //spy = spyOn(SiteService, 'getSites')
+        //    .and.returnValue(Promise.resolve(testSites));
+        //spy = spyOn(VendorService, 'loginVendor')
+        //    .and.returnValue(Promise.resolve(testLoginVendor));
         // query for the title <h1> by CSS element selector
         de = fixture.debugElement.query(platform_browser_1.By.css('animate-if vendor-gate'));
         el = de.nativeElement;
--- eCanteen/app/viewcontroller/vendor/signupSignin.component.spec.ts
+++ eCanteen/app/viewcontroller/vendor/signupSignin.component.spec.ts
@@ -1,4 +1,4 @@
-﻿import { ComponentFixture, TestBed } from '@angular/core/testing';
+﻿import { ComponentFixture, TestBed,ComponentFixtureAutoDetect } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -10,22 +10,40 @@ import { ButtonComponent } from '../../directives/button.component';
 import { SignupSinginComponent } from './signupSignin.component';
 import { SinginComponent } from './Signin.component';
 
+import { SiteService } from '../../datacontext/site.service';
+import { VendorService } from '../../datacontext/vendor.service';
+import { httpHelper } from '../../Helpers/httpHelper';
+
 describe('SignupSinginComponent (template)', () => {
 
     let comp: SignupSinginComponent;
     let fixture: ComponentFixture<SignupSinginComponent>;
     let de: DebugElement;
     let el: HTMLElement;
+    let spy: any;
+    let testSites: any;
+    let testLoginVendor: any;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [FormsModule, BrowserModule],
             declarations: [SignupSinginComponent, SinginComponent, TextboxGeneralComponent, ButtonComponent], // declare the test component
+            providers: [
+                SiteService,
+                VendorService,
+                httpHelper
+            ]
         })
 
         fixture = TestBed.createComponent(SignupSinginComponent);
 
         comp = fixture.componentInstance; // BannerComponent test instance
+
+        //spy = spyOn(SiteService, 'getSites')
+        //    .and.returnValue(Promise.resolve(testSites));
+
+        //spy = spyOn(VendorService, 'loginVendor')
+        //    .and.returnValue(Promise.resolve(testLoginVendor));
 
         // query for the title <h1> by CSS element selector
         de = fixture.debugElement.query(By.css('animate-if vendor-gate'));
